e04537b8553cf10d38d5dfa1ce700aa2f333cab7
suresh82 <madhusuresh936@gmail.com>
2020-11-05 17:35:45 +0530

    Create FirstspringbootApplicationTests.java

---
+++ FirstspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.firstspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
