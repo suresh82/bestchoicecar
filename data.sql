7ae584da0bf5321fce9b43543e2e04695e1ca437
suresh82 <madhusuresh936@gmail.com>
2020-11-05 17:20:13 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,9 @@
+insert into cars(brand,model,year,no_of_kms,price,fuel)  values('Hyundai','I20',2019,10000,'300000','Petrol');
+insert into cars(brand,model,year,no_of_kms,price,fuel)  values('Hyundai','I20',2018,20000,'200000','Diesel');
+insert into cars(brand,model,year,no_of_kms,price,fuel)  values('Hyundai','Verna',2019,15000,'250000','Petrol');
+insert into cars(brand,model,year,no_of_kms,price,fuel)  values('Tata','Bolt',2019,15000,'250000','Petrol');
+insert into cars(brand,model,year,no_of_kms,price,fuel)  values('Maruthi Suzuki','Ciaz',2019,5000,'600000','Petrol');
+insert into cars(brand,model,year,no_of_kms,price,fuel)  values('Honda','Zaz',2018,20000,'200000','Diesel');
+insert into cars(brand,model,year,no_of_kms,price,fuel)  values('Honda','City',2019,15000,'400000','Petrol');
+insert into cars(brand,model,year,no_of_kms,price,fuel)  values('Maruthi Suzuki','Ertiga',2018,10000,'550000','Petrol');
+insert into cars(brand,model,year,no_of_kms,price,fuel)  values('Tata','Nexon',2019,5000,'600000','Petrol');
